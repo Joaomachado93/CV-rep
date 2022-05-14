d29e01d9e9e56bd77f5f19a0754c8240592993ff
LAPTOP-KTPGDA7K\joaom <joaomachadoporto@hotmail.com>
2022-05-14 19:31:23 +0100

    menus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
+import { SliderComponent } from './slider/slider.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerComponent
+    BannerComponent,
+    SliderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
